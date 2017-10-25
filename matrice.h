52aa0122f920b23103aeeca582d5a24798c5fed6
info <info@pcj408.psi.ad.unistra.fr>
2017-10-25 13:16:57 +0200

    Add header file for matric

---
+++ matrice.h
@@ -0,0 +1,13 @@
+#ifndef __MATRICE_H__
+#define __MATRICE_H__
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+FILE* ouveretureMatrice(char* matrice);
+int** lectureMatrice(FILE* fdMatrice);
+
+#endif
