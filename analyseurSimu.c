74f1805ba8ea381c66b105e14981da57167caf2b
Victor <victor.constans@etu.unistra.fr>
2017-10-27 00:39:20 +0200

    Add header  file to analyseurSimu

--- analyseurSimu.c
+++ analyseurSimu.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "statGlobal.h"
-#include "trace.h"
-#include "evt.h"
-#include "analyse.h"
-
+#include "analyseurSimu.h"
 
 int main(/*TODO arg*/)
 {
@@ -13,11 +7,33 @@ int main(/*TODO arg*/)
 	struct evt* newEvt;
 
 	struct statGlobal stat;
+	stat.paquetTraite=0;
+	stat.paquetEmis=0;
+	stat.paquetRecus=0;
+	stat.paquetPerdus=0;
+	stat.nbFlux=0;
+	stat.locPerte=NULL;
+
+
+	int cmpt=0;
+	struct evt* ancienEvt;
 
 	while((newEvt=nextEvt(fdTrace))!=NULL)
 	{
+		looping:
 		analyseGlobal(newEvt,&stat);
+
+		cmpt++;
+
+		ancienEvt = newEvt;
 	}
+
+
+	printf("Paquet traite %u\nPaquet Emis %u\nPaquet Recus %u\nPaquet perdus %u\n",stat.paquetTraite,stat.paquetEmis,stat.paquetRecus,stat.paquetPerdus);
+
+	printf("cmpt %d\n",cmpt);
+
+	printf("%f %d %u",ancienEvt->temps,ancienEvt->code,ancienEvt->pid);
 
 	return 0;
 }
---
+++ analyseurSimu.h
@@ -0,0 +1,15 @@
+#ifndef __ANALYSEUR_SIMU_H__
+#define __ANALYSEUR_SIMU_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "statGlobal.h"
+#include "trace.h"
+#include "evt.h"
+#include "analyse.h"
+
+
+
+int main(/*TODO arg*/);
+
+#endif
