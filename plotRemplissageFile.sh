ef29ef9c09c6ac3221dbac2f74ec0f6dd79b7081
Victor <victor.constans@etu.unistra.fr>
2017-10-31 21:38:09 +0100

    Print all curve

--- plotRemplissageFile.sh
+++ plotRemplissageFile.sh
@@ -2,19 +2,19 @@
 
 i=2
 
+echo "set key outside" >> /tmp/remplissageFile$$
 
-#while [ $i -le 5 ]
-#do
-#	if [ $i -eq 2 ]
-#	then
-#		echo "plot \"remplissageFile.out\" u 1:$i w lp" >> /tmp/remplissageFile$$
-#		i=$(($i+1))
-#	else
-#		echo "replot \"remplissageFile.out\" u 1:$i w lp" >> /tmp/remplissageFile$$
-#		i=$(($i+1))
-#	fi
-#done
-echo "plot \"remplissageFile.out\" u 1:28 w l" >> /tmp/remplissageFile$$
+while [ $i -le 28 ]
+do
+	if [ $i -eq 2 ]
+	then
+		echo "plot \"remplissageFile.out\" u 1:$i w lp" >> /tmp/remplissageFile$$
+		i=$(($i+1))
+	else
+		echo "replot \"remplissageFile.out\" u 1:$i w lp" >> /tmp/remplissageFile$$
+		i=$(($i+1))
+	fi
+done
 echo "pause -1" >> /tmp/remplissageFile$$
 
 gnuplot /tmp/remplissageFile$$
